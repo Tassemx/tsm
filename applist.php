15cedf1a1839198ee391e64b388889b9fe009cda
Tassemx <48708805+Tassemx@users.noreply.github.com>
2019-03-19 07:47:12 +0100

    Add files via upload

---
+++ applist.php
@@ -0,0 +1,78 @@
+ [
+
+{
+                "name": "تطبيق كيف تجعل الفتاة تحبك‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.lovebnt",
+    		"iconUrl": "https://lh3.googleusercontent.com/PW5VLsSk0dv_vm-FjUmigd2GEkrn8kX9tox60x3yk-llUmMItDBx5QaK_YwzYiQvUXvu=s180-rw"     
+ },
+
+
+
+
+
+{
+                "name": "تطبيق رواية دقات الشامو - قواعد جارتين 2",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.Chamo",
+    		"iconUrl": "https://lh3.googleusercontent.com/RRVaLreWs0DXl5aKIqJL2T697jtm5y3sPnE-eu-6lefITgKu2vyj_aUXZODKfRHunaS9=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق كتاب ثاني أكسيد الحب",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.oxlove",
+    		"iconUrl": "https://lh3.googleusercontent.com/iyC-4Ch8VR26CsThZRKKIBAaN-8wFgBtEvK1-94I4DLTah6bWKjXs8q8yBqFl-GQzQ=s180-rw"
+    
+      
+ },
+
+
+{
+                "name": "تطبيق رواية ملائك نصيبين - بدون انترنت‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.Malaika",
+    		"iconUrl": "https://lh3.googleusercontent.com/VobWtRVFRsT_f-ERjTQbK9UXyL2KhVJxv53RDu4O28plVqa9uGXXoxJFkjMSr5V7ln4=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق كتاب فاتتني صلاة - بدون انترنت‎‎‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.salat",
+    		"iconUrl": "https://lh3.googleusercontent.com/WYi1qxD6OOFEZyyhPR2OoEGNlYy6usWzSvOm9yMO4KEyys6U8qJuNngvkdcto_OFXQ=s180-rw"
+    
+      
+ },
+
+
+{
+                "name": "تطبيق حصن نفسك وعائلتك - الرقية الشرعية الكاملة",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.rokaia",
+    		"iconUrl": "https://lh3.googleusercontent.com/By8silleRfStL50fUQWzIH3pFfS_cwGsik-8eDcPcpeBi1n7DK7YuFuaEvBtaaCuiuk=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق رواية ولد شعب فسبيريا - روسيا",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.roussia",
+    		"iconUrl": "https://lh3.googleusercontent.com/rMiMr94CeRX757FRjIwY8SVEGoRJIel8_r4MeDEWeZMEs-R7Ls6_R87HLaMtMuGr9xE=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق رواية لكتاب - هاينة و ابريس",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.Lktab",
+    		"iconUrl": "https://lh3.googleusercontent.com/CD7u6Igm-2a6g4gb2Nr3qg426kTBbCObLJpejfblMe1nJ9djU0NlSu5c7esCyf2L8DY=s180-rw"     
+ },
+
+{
+                "name": "تطبيق تعلم اليوغا في شهر",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.yoga",
+    		"iconUrl": "https://lh3.googleusercontent.com/w_Sa-buxm8plIFr56gHObBo10B7r5_N8v3JaroMEZouhYiRySzXkVV47N8J5-UR5jQvG=s180-rw"
+    
+      
+ }
+
+]
+        
