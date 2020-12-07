3b73072fba255b98924348492367308e289e0a0c
Tassemx <48708805+Tassemx@users.noreply.github.com>
2020-12-07 17:48:16 +0100

    Add files via upload

---
+++ appliste.php
@@ -0,0 +1,125 @@
+ [
+{
+                "name": "رواية احببتها فى انتقامى‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.loveinti9am",
+    		"iconUrl": "https://lh3.googleusercontent.com/-Ftapmn1_lPy3FiUS_rgpjrYBma9XZGAjtwXEZqf9Ywg206l75GYIz0Jr2AXfume=s360-rw"     
+ },
+{
+                "name": "رواية شهر العسل‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.Tsm.ChaharAsal",
+    		"iconUrl": "https://lh3.googleusercontent.com/8ylZuLzWr1UpnkbFBmXnOiT1Qzfb7y1dRYEwN2Fqw6EuXZrqFRhTb5V1uan-2pj07k4=s360-rw"     
+ },
+{
+                "name": "تطبيق كيف تجعل الفتاة تحبك‎",
+    		"url": "http://bit.ly/2kCoMur",
+    		"iconUrl": "https://lh3.googleusercontent.com/PW5VLsSk0dv_vm-FjUmigd2GEkrn8kX9tox60x3yk-llUmMItDBx5QaK_YwzYiQvUXvu=s180-rw"     
+ },
+
+{
+                "name": "تطبيق كتاب مميز بالأصفر 2019",
+    		"url": "http://bit.ly/2kubq3x",
+    		"iconUrl": "https://lh3.googleusercontent.com/M5alTYjSYT_RRerZws-pWx0nz9ojpsVDS5c4ftQIZSb3Y7dQs7dU7WKZxaLt6AGv2Q=s180-rw"     
+ },
+{
+                "name": "تطبيق كتاب نظرية الفستق",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.fostok",
+    		"iconUrl": "https://lh3.googleusercontent.com/a1h0ZpZgs3t7oD6HiAy-Fg-R4G7kx5WPdzVbXWjdIRAK3WczZ0qeZzXZTHViJzLxlU0U=s180-rw"     
+ },
+ 
+ {
+                "name": "تطبيق تصرفي كسيدة وفكري كرجل",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.tasrafikarajol",
+    		"iconUrl": "https://lh3.googleusercontent.com/HXT6nyBWqV_P6u_L4LCb7I2KhFiMufPwhTMMgAUmp6j9v1d9Fntctd8YhRGy6MeddtA=s180-rw"     
+ },
+ 
+  {
+                "name": "تطبيق رواية ذئاب لا تعرف الحب",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.knowlovey",
+    		"iconUrl": "https://lh3.googleusercontent.com/0JLBERbdG_eTcovJ1BhHeMH1fUJvgebPjYipYsGkwAhwkh2lq4nSaUyeSE7-JmW38kU=s180-rw"     
+ },
+ 
+   {
+                "name": "تطبيق اللص و الكلاب بدون انترنت",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.kilabwlis",
+    		"iconUrl": "https://lh3.googleusercontent.com/yHRNGfq-i5zCjv5kznrEEH8ZVCH9TK3AyRf4D3p-zd-4P1MF9fxt5ZGxAdhDlLlPjasF=s180-rw"     
+ },
+ 
+ 
+{
+                "name": "تطبيق رواية الجريمة والعقاب - جزء الأول",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.jarima",
+    		"iconUrl": "https://lh3.googleusercontent.com/DMhZHPcpNIYuCGjybQSMf9zUoVBgglaGsRoEgT_KtZbjESEwVS3JVY6PJ58ZNmAhagCX=s180-rw"     
+ },
+ 
+ {
+                "name": "تطبيق رواية الجريمة والعقاب - الجزء الثاني",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.jarimadou",
+    		"iconUrl": "https://lh3.googleusercontent.com/vz_asK_bzexp6cMnTsgkusT07fM4H_-xa4nGgZqMZOBFg6HsQoajHWq0TjWQ-EdJkQ=s180-rw"     
+ },
+ 
+{
+                "name": "تطبيق رواية دقات الشامو - قواعد جارتين 2",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.Chamo",
+    		"iconUrl": "https://lh3.googleusercontent.com/RRVaLreWs0DXl5aKIqJL2T697jtm5y3sPnE-eu-6lefITgKu2vyj_aUXZODKfRHunaS9=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق كتاب ثاني أكسيد الحب",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.oxlove",
+    		"iconUrl": "https://lh3.googleusercontent.com/iyC-4Ch8VR26CsThZRKKIBAaN-8wFgBtEvK1-94I4DLTah6bWKjXs8q8yBqFl-GQzQ=s180-rw"
+    
+      
+ },
+
+
+{
+                "name": "تطبيق رواية ملائك نصيبين - بدون انترنت‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.Malaika",
+    		"iconUrl": "https://lh3.googleusercontent.com/VobWtRVFRsT_f-ERjTQbK9UXyL2KhVJxv53RDu4O28plVqa9uGXXoxJFkjMSr5V7ln4=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق كتاب فاتتني صلاة - بدون انترنت‎‎‎",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.salat",
+    		"iconUrl": "https://lh3.googleusercontent.com/WYi1qxD6OOFEZyyhPR2OoEGNlYy6usWzSvOm9yMO4KEyys6U8qJuNngvkdcto_OFXQ=s180-rw"
+    
+      
+ },
+
+
+{
+                "name": "تطبيق حصن نفسك وعائلتك - الرقية الشرعية الكاملة",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.rokaia",
+    		"iconUrl": "https://lh3.googleusercontent.com/By8silleRfStL50fUQWzIH3pFfS_cwGsik-8eDcPcpeBi1n7DK7YuFuaEvBtaaCuiuk=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق رواية ولد شعب فسبيريا - روسيا",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.roussia",
+    		"iconUrl": "https://lh3.googleusercontent.com/rMiMr94CeRX757FRjIwY8SVEGoRJIel8_r4MeDEWeZMEs-R7Ls6_R87HLaMtMuGr9xE=s180-rw"
+    
+      
+ },
+
+{
+                "name": "تطبيق رواية لكتاب - هاينة و ابريس",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.Lktab",
+    		"iconUrl": "https://lh3.googleusercontent.com/CD7u6Igm-2a6g4gb2Nr3qg426kTBbCObLJpejfblMe1nJ9djU0NlSu5c7esCyf2L8DY=s180-rw"     
+ },
+
+{
+                "name": "تطبيق تعلم اليوغا في شهر",
+    		"url": "https://play.google.com/store/apps/details?id=com.tsm.yoga",
+    		"iconUrl": "https://lh3.googleusercontent.com/w_Sa-buxm8plIFr56gHObBo10B7r5_N8v3JaroMEZouhYiRySzXkVV47N8J5-UR5jQvG=s180-rw"
+    
+      
+ }
+
+]
+        
